4258e10e0b59a5c68dc951f17782f283b8e0f78b
Koke Thiele <kokepizzadev@gmail.com>
2025-09-09 13:57:25 +0200

    fix: set default WordPress domain for API requests

--- src/lib/wp.js
+++ src/lib/wp.js
@@ -1,4 +1,4 @@
-const domain = import.meta.env.WP_DOMAIN;
+const domain = import.meta.env.WP_DOMAIN || "https://cms.toride.es";
 const apiUrl = `${domain}/wp-json/wp/v2`;
 
 export async function getPageInfo(slug) {
