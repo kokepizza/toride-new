2896d77f528ee337283d7a04b81aa58752fcc621
Koke Thiele <kokepizzadev@gmail.com>
2025-09-10 14:34:26 +0200

    Update contact information, fetch home page data, and adjust animation duration
    - Changed contact phone number in ContactInfo component.
    - Updated index.astro to fetch data from WordPress and use ACF fields for dynamic content in HomeHero, IntroImage, Programs, Calendar, and ContactInfo components.
    - Adjusted animation duration in theme.css from 1s to 0.8s for smoother transitions.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  devToolbar: {
-    enabled: false,
-  },
-
+  devToolbar: { enabled: false },
   integrations: [react()],
 });
